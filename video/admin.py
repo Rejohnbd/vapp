7a20c01a305272f829aa4bae798ec87b09bece75
unknown <rejohn.cse@gmail.com>
2024-06-20 12:28:15 +0600

    Cutomize list & form view

--- video/admin.py
+++ video/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from . models import VideoUpload
+from . models import VideoDecode, VideoUpload
 
 # Register your models here.
-admin.site.register(VideoUpload)
+@admin.register(VideoUpload)
+class VideoUploadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+
+@admin.register(VideoDecode)
+class VideoDecodeAdmin(admin.ModelAdmin):
+     list_display = ('video_file', 'short_code', 'original_text', 'converted_text')
+     fields = ('video_file', 'short_code')
